95c87461fe2633deadecd407bd12ceb2014a808f
Michell Cronberg <michell@cronberg.dk>
2020-07-20 14:49:11 +0200

    Add project files.

---
+++ ODataDemo/Controllers/ApiController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SQLiteEF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataDemo.Controllers
+{
+    public class ApiController : ODataController
+    {
+        private readonly ILogger<ApiController> _logger;
+        private readonly SQLiteEF.PeopleContext _context;
+
+        public ApiController(ILogger<ApiController> logger, SQLiteEF.PeopleContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        
+        
+        [HttpGet]
+        [EnableQuery(PageSize = 50)]
+        [ODataRoute("people")]
+        public IQueryable<Person> GetPeople()
+        {
+            return _context.People;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        [ODataRoute("people({id})")]
+        public IActionResult GetAddresses([FromODataUri] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var people = SingleResult.Create(_context.People.Where(p => p.PersonId == id));
+            return Ok(people);
+        }
+
+    }
+}
